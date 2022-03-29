512e01eb80f1648f67e621cdb60387e7fda28eef
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-03-29 22:08:02 +0200

    node.js

--- scripts/app.mjs
+++
@@ -1,8 +0,0 @@
-// needs to be a mudule in orde to import
-import {flip as filipper , up} from './utils.mjs';
-
-function hello(nm){
-  let  name = up(filipper(nm))
-  console.log(`hallo ${name}`)
-}
-hello('Yasser')
