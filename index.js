10137492368d1321d5c43e1e223409fc83366864
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-03-31 00:28:38 +0200

    form styling

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express();
 const port = 5000;
-
+// serve public files
 app.use(express.static('public'))
+// hook up tamplats engine
 app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use('/assets', express.static('assets'))
+
+
 
 // identification route
 app.get('/',(request, response) =>{
