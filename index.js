05dea01e0a8dcdfd3d133ca3364bdbb0fb71e4fb
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-03-31 23:12:50 +0200

    node fetch

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 const express = require('express')
 const app = express();
-const port = 8001;
+const port = 7000;
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
+// const Base_url = 'https://codingthecurbs.api.fdnd.nl/v1/smartzone'
+
+
+
 // serve public files
 app.use(express.static('public'))
+app.use('/assets', express.static('assets'))
+
 // hook up tamplats engine
 app.set('view engine', 'ejs')
 app.set('views', './views')
-app.use('/assets', express.static('assets'))
+
 
 
 
 // identification route
 app.get('/',(request, response) =>{
-    response.render('pages/home')
+
+    fetchJson('https://codingthecurbs.api.fdnd.nl/v1/smartzone')
+    .then(function(JsonData) {
+        response.render('pages/informatieApi', {
+            title:'Smart Zones',
+            smartzones: JsonData.data
+        })
+    })
+    // response.render('pages/home')
 })
 
 app.listen(port, () =>{
     console.log(`listening to the port ${port}`)
 })
+async function fetchJson(url) {
+    return await fetch(url)
+      .then((response) => response.json())
+      .catch((error) => error)
+  }
